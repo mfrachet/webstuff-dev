2ee96a36a69db3f02e82f3cfc0c21e8e1c5ea2d8
mfrachet <undefined>
2023-03-13 22:26:08 +0100

    fix

--- src/modules/concepts/feature-flags/FeatureFlagExample.tsx
+++ src/modules/concepts/feature-flags/FeatureFlagExample.tsx
@@ -10,14 +10,30 @@ export function FeatureFlagExample() {
     <ExampleWrapper>
       <Canvas>
         <BrowserMoqup>
-          {checked ? <p>checked</p> : <p>not checked</p>}
+          <div className="flex flex-col gap-1">
+            {checked && (
+              <div className="bg-indigo-700 rounded-lg h-20 flex flex-col items-center justify-center gap-2">
+                <div className="bg-indigo-900 rounded-full h-4 w-2/5"></div>
+                <div className="bg-indigo-900 rounded-full h-4 w-4/5"></div>
+              </div>
+            )}
+            <div className="bg-gray-100 rounded-full h-4 w-4/5"></div>
+            <div className="bg-gray-100 rounded-full h-4 w-1/2"></div>
+            <div className="bg-gray-100 rounded-full h-4"></div>
+            <div className="bg-gray-100 rounded-full h-4"></div>
+            <div className="bg-gray-100 rounded-full h-4"></div>
+          </div>
         </BrowserMoqup>
         <BrowserMoqup>
-          <Switch
-            checked={checked}
-            onChange={setChecked}
-            label="Toggle le flag"
-          />
+          <div className="text-lg font-bold pb-2">
+            Tableau de bord des
+            <br /> feature flags
+          </div>
+
+          <div className="flex flex-row justify-between gap-2 p-2 bg-slate-50 items-center">
+            <div className="">Nouveau hero</div>
+            <Switch checked={checked} onChange={setChecked} label="Toggle" />
+          </div>
         </BrowserMoqup>
       </Canvas>
     </ExampleWrapper>
